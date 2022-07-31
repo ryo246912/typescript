df7790d8f1d7c33a7a30a52dd4f1d263477e1e2d
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2022-07-31 23:55:57 +0900

    [add]: 型
    ●型
    ・文字列
    const name2: string | null = 'John Doe';
    
    ・数字
    const age: number = 25;
    
    ・数値
    const isAdmin: boolean = true;
    
    ・配列
    ー型名[]もしくはArray<型名>で指定、Array<型名>はGenerics(ジェネリクス)
    ーUnion(|)
    const fruits: string[] = ['apple', 'banana'];
    const fruits3: (string | number)[] = ['apple', 10];
    const fruits2: Array<string | number> = ['apple', 'banana'];
    
    ・タプル
    ータプルの場合は、要素の順番も指定(:[number,string]だと×)
    const fruits_tuple:[string,number] = ['apple',10];
    
    ・null
    const value:null = null;
    
    ・undefined
    const value2 = undefined;
    
    ・any
    const name_1: any = undefined;
    
    ・オブジェクト
    const user_dict: {
      id: number;
      name: string;
    } = {
      id: 100,
      name: 'John Doe',
    };
    
    ー横1列に書く場合はセミコロンが必要
    ーoptionalプロパティ (引数名)?
    ーreadonlyプロパティ readonly(引数名)
    const user_dict2: { readonly id: number; name?: string } = {
      id: 100,
      name: 'John Doe',
    };
    
    ・関数
    ー引数と返値に型設定 (引数名:型):型
    const hello0 = (name: string): string => {
      return 'Hello ' + name;
    };
    ー関数の型を設定 [(name: string) => string]の部分
    const hello: (name: string) => string = (name: string): string => {
      return 'Hello ' + name;
    };
    
    https://reffect.co.jp/html/hello-typescript-tutorial

--- tutorial/greeter.ts
+++ tutorial/greeter.ts
@@ -3,4 +3,46 @@ function greeter(person:string) {
 }
 
 let user = "Jane User";
+
+// 型
+// 文字列
+const name2: string | null = 'John Doe';
+// 数字
+const age: number = 25;
+// 数値
+const isAdmin: boolean = true;
+// 配列
+const fruits: string[] = ['apple', 'banana'];
+const fruits3: (string | number)[] = ['apple', 10];
+const fruits2: Array<string | number> = ['apple', 'banana'];
+// タプル
+const fruits_tuple:[string,number] = ['apple',10];
+// null
+const value:null = null;
+// undefined
+const value2 = undefined;
+// any
+const name_1: any = undefined;
+// オブジェクト
+const user_dict: {
+  id: number;
+  name: string;
+} = {
+  id: 100,
+  name: 'John Doe',
+};
+const user_dict2: { readonly id: number; name?: string } = {
+  id: 100,
+  name: 'John Doe',
+};
+
+// 関数
+const hello0 = (name: string): string => {
+  return 'Hello ' + name;
+};
+// 関数の型を設定 [(name: string) => string]の部分
+const hello: (name: string) => string = (name: string): string => {
+  return 'Hello ' + name;
+};
+
 document.body.textContent = greeter(user);
