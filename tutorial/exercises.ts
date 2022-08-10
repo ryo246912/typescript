cce47bd9fe53b0843af5ac27bb987c7e8ddd5948
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2022-08-10 22:48:42 +0900

    [add]: TS型演習2
    ・declare
    関数の中身を書かずに、型註釈をすることができる
    declare function foo(arg: number): number;
    https://qiita.com/uhyo/items/e4f54ef3b87afdd65546

--- tutorial/exercises.ts
+++ tutorial/exercises.ts
@@ -39,3 +39,117 @@ const E1_4 = () => {
 
   const sum: number = sumOfPos([1, 3, -2, 0]);
 }
+
+const E2_1 = () => {
+  function myFilter<T>(arr:T[], predicate:(elm:T) => boolean):T[] {
+    const result = [];
+    for (const elm of arr) {
+      if (predicate(elm)) {
+        result.push(elm);
+      }
+    }
+    return result;
+  }
+
+  const res = myFilter([1, 2, 3, 4, 5], num => num % 2 === 0);
+  const res2 = myFilter(['foo', 'hoge', 'bar'], str => str.length >= 4);
+  const res3 = myFilter([['foo'], ['foo', 'hoge'], ['foo', 'hoge', 'bar']], num => num.length >= 2);
+}
+
+const E2_2 = () => {
+  type Speed = "slow" | "medium" | "fast";
+
+  function getSpeed(speed: Speed): number {
+    switch (speed) {
+      case "slow":
+        return 10;
+      case "medium":
+        return 50;
+      case "fast":
+        return 200;
+    }
+  }
+
+  const slowSpeed = getSpeed("slow");
+  const mediumSpeed = getSpeed("medium");
+  const fastSpeed = getSpeed("fast");
+}
+
+const E2_3 = () => {
+// 型註釈
+  type OptionalOb = {
+    capture?:boolean;
+    once?:boolean;
+    passive?:boolean;
+  }
+
+  // FIXME ここで修飾子を使用することはできません。
+  declare function addEventListener  (
+    s: string,
+    func: (x: any) => void,
+    param?: boolean | OptionalOb
+  ) : void;
+
+  addEventListener("foobar", () => {});
+  addEventListener("event", () => {}, true);
+  addEventListener("event2", () => {}, {});
+  addEventListener("event3", () => {}, {
+    capture: true,
+    once: false
+  });
+
+}
+
+const E2_4 = () => {
+
+  type Obj = {
+    id:string;
+  }
+
+  function giveId<T>(obj:T):T & Obj {
+    const id = "string";
+    return {
+      ...obj,
+      id
+    };
+  }
+
+  const obj1: {
+    id: string;
+    foo: number;
+  } = giveId({ foo: 123 });
+  const obj2: {
+    id: string;
+    num: number;
+    hoge: boolean;
+  } = giveId({
+    num: 0,
+    hoge: true
+  });
+
+}
+
+const E2_5 = () => {
+
+  declare function useState<T>(init: T): [T, (y: T | ((z: T) => T)) => void];
+  // ↓見栄えが悪いので関数の引数は分ける
+  type UseStateUpdateArgument<T> = T | ((oldValue: T) => T);
+  declare function useState2<T>(
+    initialValue: T
+  ): [T, (updator: UseStateUpdateArgument<T>) => void];
+
+  // number型のステートを宣言 (numStateはnumber型)
+  const [numState, setNumState] = useState(0);
+  // setNumStateは新しい値で呼び出せる
+  setNumState(3);
+  // setNumStateは古いステートを新しいステートに変換する関数を渡すこともできる
+  setNumState((state) => state + 10);
+
+  // 実際のReact・useStateの実装
+  // function useState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
+  // function useState<S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>];
+
+}
+
+const E = () => {
+}
